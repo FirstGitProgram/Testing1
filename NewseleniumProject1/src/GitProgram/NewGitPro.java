4e7bfabde8fba271bf32cd82e472d12857dd5db4
FirstGitProgram <99386749+FirstGitProgram@users.noreply.github.com>
2022-02-10 20:58:48 +0530

    By Nilu

--- NewseleniumProject1/src/GitProgram/NewGitPro.java
+++ NewseleniumProject1/src/GitProgram/NewGitPro.java
@@ -3,9 +3,15 @@ package GitProgram;
 public class NewGitPro 
 
 {
+	public void m1()
+	{
+		 System.out.println("m1 Method");
+	}
+	
      public static void main(String[] args)
      {
     	 System.out.println("Hello Git Program");
 		
-	}
+    }
+	
 }
