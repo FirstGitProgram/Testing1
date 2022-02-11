acff4d02eb636adbc750339ab9c2fec3d5c3096b
FirstGitProgram <99386749+FirstGitProgram@users.noreply.github.com>
2022-02-11 18:47:38 +0530

    Update DemoProgram.java

--- NewseleniumProject1/src/GitProgram/DemoProgram.java
+++ NewseleniumProject1/src/GitProgram/DemoProgram.java
@@ -1,7 +1,12 @@
 package GitProgram;
 
 public class DemoProgram 
+	
 {
+	public void M1()
+	{
+		System.out.println("Hello M1 Method")
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("This is my new Github program");
